5923f2833cc13cbd8e669f05ecf56071a106d37c
Yale Kaul <yalestar@gmail.com>
2011-06-19 16:30:18 -0700

    initial rails generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SafeEatsPDX::Application.config.secret_token = '96e351e28976bc3cb5d4dea3373ceaf42a301399d47a00c6607a10576c4e1bce063d968f2010f6948f6c0f33766fea843a775d0dcda332a5370cb6c38c934713'
