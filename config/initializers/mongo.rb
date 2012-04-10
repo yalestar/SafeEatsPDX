612c76f1e8a51357ff3e226e1df6cf1b752978ab
yalestar <yalestar@gmail.com>
2012-04-10 10:03:17 -0700

    a little env conf

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,15 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "safe_eats_pdx"
+if Rails.env == "development"
+	MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
+	MongoMapper.database = "safe_eats_pdx"
+
+elsif Rails.env == "production"
+	MongoMapper.connection = Mongo::Connection.new('flame.mongohq.com', 27052)
+	db = MongoMapper.database = "safe_eats_pdx"
+	auth = db.authenticate("yalestar", "eldongo1")
+
+end
+		
+# mongodb://<user>:<password>@flame.mongohq.com:27052/app3879091
 # MongoMapper.database = "pdx"
 
 if defined?(PhusionPassenger)
